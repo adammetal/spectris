81157f853caf3567000189f3b11255b2cc43b55e
adam.metal <adam.metal@codecool.com>
2022-04-22 14:43:42 +0200

    handle remove injectable files

--- src/components/Controls/Options.jsx
+++ src/components/Controls/Options.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import Injectables from './Injectables';
-import { addCss, addJs } from '../../redux/features/injector';
+import { addCss, addJs, removeCss, removeJs } from '../../redux/features/injector';
 
 function Options() {
   const { css, js } = useSelector((state) => state.injector);
@@ -20,18 +20,28 @@ function Options() {
     }
   };
 
+  const handleRemoveCssFile = (file) => {
+    dispatch(removeCss(file));
+  };
+
+  const handleRemoveJsFile = (file) => {
+    dispatch(removeJs(file));
+  };
+
   return (
     <div className="flex gap-4 w-8/12">
       <Injectables
         files={css}
         title="Extra Css styles"
         onSelectNew={handleSelectCssFile}
+        onRemove={handleRemoveCssFile}
         hint="When the inspector opens, you can inject extra css files. They will applied to the inspected game."
       />
       <Injectables
         files={js}
         title="Extra JavaScript codes"
         onSelectNew={handleSelectJsFile}
+        onRemove={handleRemoveJsFile}
         hint="When the inspector opens, you can inject extra javascript files. The scripts will start runnig shortly after the inspector loads the game."
       />
     </div>
