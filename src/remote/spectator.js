967dcaf5a9f88382f733e0b971ad3c6a2ab2f59a
adam.metal <adam.metal@codecool.com>
2022-05-18 10:37:26 +0200

    add basic logger to express

--- src/remote/spectator.js
+++ src/remote/spectator.js
@@ -7,6 +7,12 @@ const server = express();
 
 server.use(express.json());
 
+server.use((req, res, next) => {
+  console.log('request incoming');
+  console.log(req.method, req.url);
+  next();
+});
+
 server.put('/spectate', (req, res) => {
   const { gameUrl } = req.body;
   remote.emit('spectate', gameUrl);
