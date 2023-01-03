b3e6d2736a90a067327b4415f2f6c72a1a95c6ce
adam.metal <adam.metal@codecool.com>
2023-01-03 11:42:37 +0100

    change the font size of panels

--- src/components/ui-kit/Panel.jsx
+++ src/components/ui-kit/Panel.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Panel({ children, title }) {
   return (
     <div className="bg-blue-200 shadow-xl text-black border-4 border-pink-500 rounded-lg w-6/12">
-      <div className="pl-2 bg-pink-400 text-white text-[1.5rem]">{title}</div>
+      <div className="pl-2 bg-pink-400 text-white text-[1.8rem]">{title}</div>
       <div className="p-2">{children}</div>
     </div>
   );
